07fe121303d4b3fb7cb80abb8a1b645b27c615f1
Alberto Araújo <albertoafaj@hotmail.com>
2023-12-07 11:12:26 -0300

    test: use theory and InlineData notations to test various scenarios

--- Alura.Estacionamento.Testes/PatioTestes.cs
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -3,6 +3,7 @@ using Alura.Estacionamento.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -31,5 +32,34 @@ namespace Alura.Estacionamento.Testes
             //Assert 
             Assert.Equal(2, faturamento);
         }
+
+        [Theory]
+        [InlineData("Alberto Araujo", "ASD-9999", "Verde", "Fusca")]
+
+        [InlineData("Tadashi Araujo", "ASD-7777", "Amarela", "Lamborguine")]
+
+        [InlineData("Bernando Araujo", "ASD-8888", "Vermelha", "Ferrari")]
+        public void ValidaFaturamentoComVariosVeiculos(string proprietario, string placa, string cor, string modelo)
+        {
+            //Arrange
+            Patio estacionamento = new Patio();
+            Veiculo veiculo = new Veiculo();
+            veiculo.Proprietario = proprietario;
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+            veiculo.Placa = placa;
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act 
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+
+        }
     }
 }
+
